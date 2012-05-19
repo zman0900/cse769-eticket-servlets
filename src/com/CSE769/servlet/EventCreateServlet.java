6b019b31422179dded592f5cddcfdb7955ea455d
Sandeep <crsandy@gmail.com>
2012-05-19 00:55:36 -0400

    Event creation page. Some issue still

---
+++ src/com/CSE769/servlet/EventCreateServlet.java
@@ -0,0 +1,81 @@
+package com.CSE769.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.ejb.EJB;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.cse769.EJB.Entity.Event;
+import com.cse769.EJB.Entity.EventCategory;
+import com.cse769.EJB.Entity.Venue;
+import com.cse769.EJB.Service.EventCategoryService;
+import com.cse769.EJB.Service.EventService;
+import com.cse769.EJB.Service.VenueService;
+
+/**
+ * Servlet implementation class EventCreate
+ */
+@WebServlet("/EventCreateServlet")
+public class EventCreateServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public EventCreateServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+    
+    @EJB
+    EventService eventService = new EventService();
+    
+    @EJB
+    EventCategoryService eventCategoryService = new EventCategoryService();
+    
+    @EJB
+    VenueService venueService = new VenueService();
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String name = request.getParameter("name");
+		String category = request.getParameter("category");
+		String description = request.getParameter("description");
+		String cost = request.getParameter("cost");
+		String date = request.getParameter("date");
+		String quantity = request.getParameter("quantity");
+		String venue = request.getParameter("venue");
+		
+		List<EventCategory> eventCategory = new ArrayList<EventCategory>();
+		List<EventCategory> cat = eventCategoryService.findCategoryByName(category);
+		eventCategory.add((EventCategory) cat);
+		
+		List<Venue> venueObject = venueService.findVenuesByName(venue);
+		
+		eventService.createEvent(name, eventCategory, description, new Double(cost), new Date(), new Integer(quantity), venueObject.get(0));
+		
+		PrintWriter writer = response.getWriter();		
+		List<Event> events = eventService.getAllEvents();     
+		for (Event event : events) {
+			writer.println("<h1>" + event.getName() +  "</h1>");
+		}		
+	}
+
+}
