df555a7e7fd2efac95e021c6f227deb0e5161389
Sandeep <crsandy@gmail.com>
2012-05-18 20:36:37 -0400

    Venue

--- src/com/CSE769/servlet/VenueServlet.java
+++ src/com/CSE769/servlet/VenueServlet.java
@@ -2,6 +2,7 @@ package com.CSE769.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
 
 import javax.ejb.EJB;
 import javax.servlet.ServletException;
@@ -10,7 +11,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.cse769.EJB.Service.UserService;
+import com.cse769.EJB.Entity.Venue;
 import com.cse769.EJB.Service.VenueService;
 
 /**
@@ -27,22 +28,36 @@ public class VenueServlet extends HttpServlet {
 		super();
 		// TODO Auto-generated constructor stub
 	}
+	
+	static String PAGE_HEADER = "<html><head /><body>";
 
-	@EJB
-	private UserService userService;
+	static String PAGE_FOOTER = "</body></html>";
 
 	@EJB
 	private VenueService venueService;
-
+	
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
 		PrintWriter writer = resp.getWriter();
-		userService.createUser("Sandeep", "passw0rd", "crsandy@gmail.com",
+		/*userService.createUser("Sandeep", "passw0rd", "crsandy@gmail.com",
 				"1444 Worthington Street", "Columbus", "Ohio", "43201",
-				"6145863484");
+				"6145863484", null, null);
 		venueService.createVenue("Independence day", 10,
 				"Indian Independence day", "Oval OSU", "Columbus", "Ohio",
-				"43210");
+				"43210", null);*/
+		List<Venue> venues = venueService.getAllVenues();
+		List<Venue> venues2 = venueService.findVenuesByName("ABC");
+		
+		writer.println(PAGE_HEADER);
+	       
+		for (Venue venue : venues) {
+			writer.println("<h1>" + venue.getName() +  "</h1>");
+		}
+		
+		for (Venue venue : venues2) {
+			writer.println("<h3>" + venue.getName() +  "</h3>");
+		}
+		writer.println(PAGE_FOOTER);
 		writer.close();
 	}
 
@@ -58,6 +73,6 @@ public class VenueServlet extends HttpServlet {
 		String zipcode = req.getParameter("Zipcode");
 		
 		venueService.createVenue(name, new Integer(size), description, address,
-				city, state, zipcode);
+				city, state, zipcode, null);
 	}
 }
